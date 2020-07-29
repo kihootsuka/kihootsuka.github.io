028acd909348ddc5e5de8798a7f151dbc7f840a8
kihootsuka <68770517+kihootsuka@users.noreply.github.com>
2020-07-29 16:48:38 +0900

    Add files via upload

---
+++ kadai3last/js/final.js
@@ -0,0 +1,8 @@
+
+
+var start_call=["私のプログラミングの最終課題ページへようこそ！"];
+for(var i =0; i < start_call.length; i++){
+    alert(start_call[i]);
+}
+
+
---
+++ kadai3last/js/final1.js
@@ -0,0 +1,31 @@
+function hello(language){
+    
+    var message = "";
+    
+    if(language ==1){
+        message="はずれです！もう一度！";
+    } else if (language==2){
+        message="はずれです！もう一度！";
+    } else if (language ==3){
+        message="正解です！他の大学は課題が前より増えたようですが、私のところは元から出席確認のための小レポートがあるなど、課題がとっても多い大学です。";
+    } else if (language ==4){
+        message="はずれです！もう一度！";
+    } else if (language ==5){
+        message="はずれです！もう一度！";
+    } else if (language ==6){
+        message="正解です！個人的には変わらなかったというだけで、周りは通学時間がないぶん授業の数を増やして取っていたそうです。";
+    } else if (language==7){
+        message="はずれです！そうだったら良かった！";
+    } else if (language==8){
+        message="正解です！みんなストレスが溜まってしまい、ぶつかることが多くなりました。";
+    } else if (language==9){
+        message="はずれです！もう一度！";
+    } else if (language==10){
+        message="正解です！出費は食費のみ、自炊のみの生活になったので貯金は増えました。";
+    } else if (language==11){
+        message="はずれです！もう一度！";
+    } else if (language==2){
+        message="はずれです！もう一度！";
+    } 
+    alert(message);
+}
